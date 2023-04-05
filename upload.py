186f0417a400444abdbe7cf1d962cbe617d10ae8
koala113 <federicobai1016@gmail.com>
2023-04-05 11:04:59 -0700

    upload

--- upload.py
+++ upload.py
@@ -2,6 +2,7 @@ import whisper
 import numpy as np
 from flask import * 
 import os
+from stable_whisper import stabilize_timestamps
 app = Flask(__name__)  
 # curr_dir = os.path.dirname(os.getcwd())
 # model = whisper.load_model(curr_dir + "/pytorch_model.bin") 
@@ -19,9 +20,13 @@ def success():
     if request.method == 'POST':  
         f = request.files['file'] 
         f.save(f.filename) 
-        result = model.transcribe(f.filename)
+        result = model.transcribe(f.filename, suppress_silence=True, ts_num=16, lower_quantile=0.05, lower_threshold=0.1)
+        stab_segments = result['segments']
+        first_segment_word_timestamps = stab_segments[0]['whole_word_timestamps']
+        stab_segments = stabilize_timestamps(result, top_focus=True)
+        print(first_segment_word_timestamps)
+        print(stab_segments)
         # audio = whisper.load_audio(f.filename)
-        # print("dfjkadj;fj;aj;fjajfka", audio)
         # audio = whisper.pad_or_trim(audio) 
         # mel = whisper.log_mel_spectrogram(audio).to(model.device)
         # _, probs = model.detect_language(mel)
