c9664c58947e5dfd349a28b85ace27b25a6ec6c3
koala113 <federicobai1016@gmail.com>
2023-04-05 09:47:09 -0700

    upload

--- upload.py
+++ upload.py
@@ -21,6 +21,7 @@ def success():
         audio = whisper.load_audio(f.filename)
         audio = whisper.pad_or_trim(audio) 
         mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        _, probs = model.detect_language(mel)
         options = whisper.DecodingOptions()
         result = whisper.decode(model, mel, options)
         # f.save(f.filename)  
