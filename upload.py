93310a9d09871254c1bed81b516f8991e0594df4
koala113 <federicobai1016@gmail.com>
2023-04-05 09:03:03 -0700

    upload

--- upload.py
+++ upload.py
@@ -13,13 +13,13 @@ def upload():
 def success():  
     if request.method == 'POST':  
         f = request.files['file'] 
-        audio = whisper.load_audio(f)
-        audio = whisper.pad_or_trim(audio) 
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
-        options = whisper.DecodingOptions()
-        result = whisper.decode(model, mel, options)
+        # audio = whisper.load_audio(f)
+        # audio = whisper.pad_or_trim(audio) 
+        # mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        # options = whisper.DecodingOptions()
+        # result = whisper.decode(model, mel, options)
         #f.save(f.filename)  
-        return render_template("index.html", name = result.text)  
+        return render_template("index.html", name = f.filename)  
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', debug = True)  
