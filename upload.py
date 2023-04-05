baabc980582f994d144e091925838c16f9a007c1
koala113 <federicobai1016@gmail.com>
2023-04-05 09:29:53 -0700

    upload

--- upload.py
+++ upload.py
@@ -1,9 +1,14 @@
 import whisper
+import numpy as np
 from flask import * 
 import os
 app = Flask(__name__)  
-curr_dir = os.path.dirname(os.getcwd())
-model = whisper.load_model(curr_dir + "/pytorch_model.bin") 
+# curr_dir = os.path.dirname(os.getcwd())
+# model = whisper.load_model(curr_dir + "/pytorch_model.bin") 
+with open('../pytorch_model.bin', 'rb') as f1:
+    data = np.fromfile(f1, dtype=np.float32)
+model = whisper.Model()
+model.load_from_bytes(data.tobytes())
 
 @app.route('/')  
 def upload():  
@@ -19,7 +24,7 @@ def success():
         # options = whisper.DecodingOptions()
         # result = whisper.decode(model, mel, options)
         #f.save(f.filename)  
-        return render_template("index.html", name = curr_dir)  
+        return render_template("index.html", name = f.filename)  
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', debug = True)  
