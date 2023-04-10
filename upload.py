f0e9ffe4fd0a88b6701b266e369ab72964044ff8
koala113 <federicobai1016@gmail.com>
2023-04-10 11:13:35 -0700

    upload.py

--- upload.py
+++ upload.py
@@ -85,7 +85,14 @@ def success():
         # options = whisper.DecodingOptions()
         # result = whisper.decode(model, mel, options)
         # f.save(f.filename)  
-        return render_template("index.html", name = result['text'], timestam = print_list)  
+        data = {
+            'result' : result['text'],
+            'timestam' : print_list
+        }
+        json_data = json.dumps(data)
+        response = Response(json_data, content_type = 'application/json')
+        return response
+        # return render_template("index.html", name = result['text'], timestam = print_list)  
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', port='3000', debug = True)  
